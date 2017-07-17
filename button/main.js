62100088005299f642df439031066534dba9e828
GreenLantern101 <arctangent20@yahoo.com>
2017-07-16 20:53:09 -0500

    fix

--- button/main.js
+++ button/main.js
@@ -4,7 +4,7 @@ var s = ['get','Element','By','Id'];
 s = s.reduce((t, e, i) => t+=e);
 var g = d[s].bind(d);
 var increment = () => {
-  var l = Object.keys(s);
+  var l = Object.keys(s).length;
   num+= l/14;
   g('count').innerHTML = num;
 }
