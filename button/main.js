c6c22a919dda0d25c555d177bae7851af59acefd
GreenLantern101 <arctangent20@yahoo.com>
2017-07-16 22:39:45 -0500

    hizenyousuck

--- button/main.js
+++ button/main.js
@@ -6,11 +6,16 @@ var m = d;
 n = ["t", "y"];
 p = ['tl', 'gt', 'mn', 'lm'];
 var s = ['gtlmnt','b', 'y','i', 'd'];
+var cover = (str) => {
+  return str.split('').reverse().join('');
+}
+var v = [cover('htgnel')];
+s.concat([cover('diyb')]);
 s[0] = s.map((e, i)=> !i*s.length ? e : 0).filter(e=>e)
 .map(e=> {
   p.forEach(h=> {
     var lol = e.indexOf(h);
-    lol+1 ? e = e.substr(0, lol+1) + String.fromCharCode(101) + e.substr(lol+1, e.length) : e = e;
+    lol+1 ? e = e.substr(0, lol+1) + String.fromCharCode(101) + e.substr(lol+1, e[v[0]]) : e = e;
   });
   return e;
 });
@@ -19,16 +24,16 @@ var k = s;
 var acc = 0;
 n.map(e => {
   do{
-    var i = k.indexOf(e)+[[]].length;
-    s = s.substr(0, acc + i) + k.charAt(i).toUpperCase() + s.substr(acc+i+1, s.length);
-    k = k.slice(i, k.length);
+    var i = k.indexOf(e)+[[]][v[0]];
+    s = s.substr(0, acc + i) + k.charAt(i).toUpperCase() + s.substr(acc+i+1, s[v[0]]);
+    k = k.slice(i, k[v[0]]);
     acc +=i;
   }
   while(k.indexOf(e)>=0)
 });
 var g = d[s].bind(d);
 var increment = () => {
-  var l = Object.keys(s).length;
+  var l = Object.keys(s)[v[0]];
   num+= l/14;
   g('count').innerHTML = num;
 }
