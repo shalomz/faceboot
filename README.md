c9cb20ea70474efc0a767131e595d7799d431f7a
GreenLantern101 <arctangent20@yahoo.com>
2017-07-16 22:07:21 -0500

    Remove getelementbyid

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # faceboot
 An obfuscated incremental game.
+
+Rules:
+
+1. No warnings/errors in the console.
+2. Counter must work as expected.
--- button/main.js
+++ button/main.js
@@ -2,8 +2,30 @@ var n = {'date': (new Date()).getUTCDate()}, num;
 for(var o in n)
   num = parseInt(+(~~!Math.floor(o)==o));
 var d = document;
-var s = ['get','Element','By','Id'];
+var m = d;
+n = ["t", "y"];
+p = ['tl', 'gt', 'mn', 'lm'];
+var s = ['gtlmnt','b', 'y','i', 'd'];
+s[0] = s.map((e, i)=> !i*s.length ? e : 0).filter(e=>e)
+.map(e=> {
+  p.forEach(h=> {
+    var lol = e.indexOf(h);
+    lol+1 ? e = e.substr(0, lol+1) + String.fromCharCode(101) + e.substr(lol+1, e.length) : e = e;
+  });
+  return e;
+});
 s = s.reduce((t, e, i) => t+=e);
+var k = s;
+var acc = 0;
+n.map(e => {
+  do{
+    var i = k.indexOf(e)+[[]].length;
+    s = s.substr(0, acc + i) + k.charAt(i).toUpperCase() + s.substr(acc+i+1, s.length);
+    k = k.slice(i, k.length);
+    acc +=i;
+  }
+  while(k.indexOf(e)>=0)
+});
 var g = d[s].bind(d);
 var increment = () => {
   var l = Object.keys(s).length;
